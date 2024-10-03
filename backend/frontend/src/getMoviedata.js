b390709c67f569bf73ddc50d9be005f35cbda983
Nitin Srivastva <nitinsrivastava2all@gmail.com>
2024-10-03 23:48:00 +0530

    update change

--- backend/frontend/src/getMoviedata.js
+++ backend/frontend/src/getMoviedata.js
@@ -4,7 +4,7 @@ function getMovieData(name){
     const [data, setData] = useState([]);
     useEffect(() => {
         async function fetchData(){
-            const response = await fetch(`http://www.omdbapi.com/?s=${name}&apikey=4bc6021a`);
+            const response = await fetch(`https://www.omdbapi.com/?s=${name}&apikey=4bc6021a`);
             const val = await response.json();
             const result = await val.Search;
             setData(result);
